8286f08a68c992a72b9d213988c43d4d5284b16f
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2019-02-08 23:21:01 -0500

    editor workaround

--- src/App.js
+++ src/App.js
@@ -3,26 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import './layout.js';
 
-import brace from 'brace';
-import AceEditor from 'react-ace';
+import CodeEditor from './codeeditor';
 
 import 'brace/mode/python';
 import 'brace/theme/github';
 
 class App extends Component {
-    codeOnChange(newValue) {
-        console.log(newValue);
-    }
+    componentDidMount() {}
     render() {
         return (
             <div className="App">
-                <AceEditor
-                    mode="python"
-                    theme="github"
-                    onChange={this.codeOnChange}
-                    name="editor"
-                    editorProps={{ $blockScrolling: true }}
-                />
+                <CodeEditor />
             </div>
         );
     }
