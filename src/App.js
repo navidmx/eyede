39bfca95855ec5cb9961f94259e694bacccf13a0
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2019-02-08 22:34:01 -0500

    Text editor

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 import './layout.js';
 
+import brace from 'brace';
+import AceEditor from 'react-ace';
+
+import 'brace/mode/python';
+import 'brace/theme/github';
+
 class App extends Component {
+    codeOnChange(newValue) {
+        console.log(newValue);
+    }
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+                <AceEditor
+                    mode="python"
+                    theme="github"
+                    onChange={this.codeOnChange}
+                    name="editor"
+                    editorProps={{ $blockScrolling: true }}
+                />
             </div>
         );
     }
