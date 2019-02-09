fd8895ce7d26a3d5178f31fba2a29c8a1484a4b6
navidmx <navidmx@gmail.com>
2019-02-08 23:34:49 -0500

    Added SASS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './layout.js';
+import './App.scss';
 
 import CodeEditor from './codeeditor';
 
